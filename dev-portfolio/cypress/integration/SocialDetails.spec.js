bb9bee2458e23d15bee3b0c4ae4308a6ebd30c76
Esteban O <estebanusco@gmail.com>
2022-11-13 19:44:46 -0800

    visual test fix

--- dev-portfolio/cypress/integration/SocialDetails.spec.js
+++ dev-portfolio/cypress/integration/SocialDetails.spec.js
@@ -1,24 +1,25 @@
 describe('SocialDetails', () => {
   beforeEach(() => {
-    cy.visit('/');
-  });
+    cy.visit('/')
+  })
 
   it('have correct social links', () => {
     cy.get("[data-testid='github']").should(
       'have.attr',
       'href',
       'https://github.com/banchito'
-    );
+    )
     cy.get("[data-testid='linkedin']").should(
       'have.attr',
       'href',
       'https://www.linkedin.com/in/banchito/'
-    );
+    )
 
     cy.get("[data-testid='source-code']").should(
       'have.attr',
       'href',
-      'https://github.com/banchito/Portfolio'
-    );
-  });
-});
+      'https://github.com/banchito/Portfolio' ||
+        'https://world-cup-alpha.vercel.app/'
+    )
+  })
+})
